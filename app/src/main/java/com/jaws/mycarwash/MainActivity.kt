4669a9c8dab62240a6b9f99237ac182eade72d18
yudiatmoko <yudiatmokoilham@gmail.com>
2023-09-02 00:26:45 +0700

    Create profile and banner

--- app/src/main/java/com/jaws/mycarwash/MainActivity.kt
+++ app/src/main/java/com/jaws/mycarwash/MainActivity.kt
@@ -2,10 +2,13 @@ package com.jaws.mycarwash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<ImageView>(R.id.iv_banner).setColorFilter(150)
     }
 }
