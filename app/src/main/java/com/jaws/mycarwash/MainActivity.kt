b8b361d08cbe35d029b8e34094fe5983510f69c8
yudiatmoko <yudiatmokoilham@gmail.com>
2023-09-02 11:33:31 +0700

    Finished the page

--- app/src/main/java/com/jaws/mycarwash/MainActivity.kt
+++ app/src/main/java/com/jaws/mycarwash/MainActivity.kt
@@ -9,6 +9,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<ImageView>(R.id.iv_banner).setColorFilter(150)
     }
 }
